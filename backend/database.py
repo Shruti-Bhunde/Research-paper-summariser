b42a28a79482e4da05c96656e0643223f39b1ea0
tech10maniac <shrutibhunde10@gmail.com>
2026-06-13 23:22:26 +0530

    added production details

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,21 @@ import mysql.connector
 import os
 
 def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DATABASE"),
-        port=os.getenv("DB_PORT") #for deployment
-    )
+
+    if os.getenv("ENV") == "production":
+        return mysql.connector.connect(
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+            unix_socket=os.getenv("CLOUD_SQL_SOCKET")
+        )
+
+    else:
+        return mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+            port=os.getenv("DB_PORT")
+        )
 
